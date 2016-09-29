9931a34ebecc51eefa51240c52dca25d08d7c724
Josef21296 <jcasanovasv@hotmail.com>
2016-09-29 21:01:27 +0200

    2projects and kinematics done

---
+++ Class01/Project/Class01-Steering/Assets/Kinematic/KinematicFlee.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinematicFlee : MonoBehaviour {
+
+	Move move;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        // TODO 6: To create flee just switch the direction to go
+        Vector3 dif = transform.position - move.target.transform.position;
+        dif.Normalize();
+        dif *= move.max_mov_velocity;
+
+        move.SetMovementVelocity(dif);
+    }
+}
---
+++ Class01/Project/Class01-Steering/Assets/Kinematic/KinematicSeek.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinematicSeek : MonoBehaviour {
+
+	Move move;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        // TODO 5: Set movement velocity to max speed in the direction of the target
+        Vector3 dif = move.target.transform.position - transform.position;
+        dif.Normalize();
+        dif *= move.max_mov_velocity;
+
+        move.SetMovementVelocity(dif);
+	}
+}
